63cb65a13d204cb9efa030fd12bd92b8ffb47e33
Amats817 <mjgstodomingo@gmail.com>
2025-06-02 17:46:44 -0500

    added jobs page

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   AOS.init({ once: true });
 
   new Typed('.hero-subtitle', {
-    strings: ['Web Developer', 'UI Designer', 'Front End Engineer'],
+    strings: ['Web Developer', 'IT Specialist', 'UI Designer', 'Full Stack Engineer'],
     typeSpeed: 60,
     backSpeed: 30,
     loop: true
